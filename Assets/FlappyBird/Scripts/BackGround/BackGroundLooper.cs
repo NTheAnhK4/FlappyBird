76cc6a5d7db447fce0c73c5a5845d628b9453fbc
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2024-07-18 14:57:08 +0700

    Start background

---
+++ Assets/FlappyBird/Scripts/BackGround/BackGroundLooper.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+
+public class BackGroundLooper : MonoBehaviour
+{
+   public float speed = -0.1f;
+   private Vector2 offset = Vector2.zero;
+   private Material material;
+   private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+
+   private void Start()
+   {
+      material = GetComponent<Renderer>().material;
+      offset = material.GetTextureOffset(MainTex);
+   }
+
+   private void Update()
+   {
+      offset += Vector2.left * (speed * Time.deltaTime);
+      material.SetTextureOffset(MainTex, offset);
+   }
+}
